74a5800c21c34fb098cce5d48e77926e0de5cc6f
Haroun Serang <haroun@ripple.com>
2015-01-14 18:18:02 -0800

    [TASK] Hacked Redirect to payments

--- app/scripts/components/router.jsx
+++ app/scripts/components/router.jsx
@@ -144,7 +144,7 @@ var routes = (
     <Route name="payments" path="payments/:direction/:state" handler={PaymentsModule}/>
     <Route name="notFound" handler={NotFound} />
     <NotFoundRoute handler={NotFound} />
-    <Redirect from="/" to={loginPath} />
+    <Redirect from="/" to="/payments/outgoing/all" />
   </Route>
 );
 
