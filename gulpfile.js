dd13bc9f36666f1f6bfa1b0af64754b149e66b30
Haroun Serang <haroun@ripple.com>
2015-02-09 15:57:44 -0800

    [FEATURE] Enable ES6 Reactify

--- gulpfile.js
+++ gulpfile.js
@@ -33,6 +33,9 @@ var merge = require('merge-stream');
 var browserify = require('browserify');
 var del = require('del');
 var reactify = require('reactify');
+var reactifyES6 = function(file) {
+  return reactify(file, {'es6': true});
+};
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var uglify = require('gulp-uglify');
@@ -278,7 +281,7 @@ gulp.task('js', function() {
 
   // Browserify/bundle the JS.
   return browserify(paths.main_js)
-    .transform(reactify)
+    .transform(reactifyES6)
     .bundle()
     .pipe(source('bundle.js'))
     .pipe(buffer())
